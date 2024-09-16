55b9dfb8d5e0791bbcbba51834272350d7d7e465
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2024-09-16 19:58:32 +0530

    category section completed
    meals cards created

--- quickdish-client/src/pages/home/Home.jsx
+++ quickdish-client/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Banner from '../../components/Banner';
+import { Categories } from './Categories';
 
 const Home = () => {
   return (
-    <div><Banner/></div>
+    <div>
+      <Banner/>
+      <Categories/>
+    
+    </div>
+
   )
 }
 
