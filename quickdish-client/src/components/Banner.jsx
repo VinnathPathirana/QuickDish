f3c0308713486b9eb48e44a35d43032203bde307
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2024-09-16 18:25:03 +0530

    Edit Nav bar
    fixed navigation bar and add home page banner

---
+++ quickdish-client/src/components/Banner.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import banner from "/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+
+ {/* Right */}
+ <div className="md:w-1/2">
+          <img src="banner.png" />
+
+          <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
+            <div className="flex bg-white py-2 px-3 rounded-3xl item-center gap-3 shadow-md w-64">
+              <img src="/b-food1.png" className="rounded-2xl" />
+              <div className="space-y-1">
+                <h5 className="font font-medium mb-1">Spicy Noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+               <p className = "text-red">$18.00</p>
+              </div>
+            </div>
+            <div className="md:flex hidden bg-white py-2 px-3 rounded-3xl item-center gap-3 shadow-md w-64">
+              <img src="/b-food1.png" className="rounded-2xl" />
+              <div className="space-y-1">
+                <h5 className="font font-medium mb-1">Spicy Noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+               <p className = "text-red">$18.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="md:w-1/2 space-y-7 px-3">
+          {/* Left */}
+          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+            Dive into Delights Of Delectable{" "}
+            <span className="text-green">Food</span>
+          </h2>
+          <p className="text-lg text-[#4A4A4A]">
+            Where Each Plate Weaves a Story of Culinary Mastery and Passionate
+            Craftsmanship{" "}
+          </p>
+          <button className="btn bg-green px-8 py-3 font-semibold text-white rounded-full">
+            Order Now
+          </button>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- quickdish-client/src/components/Navbar.jsx
+++ quickdish-client/src/components/Navbar.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "/logo.png";
+import { TbPhoneCall } from "react-icons/tb";
 
 export const Navbar = () => {
+  const [isSticky , setSticky] = useState(false);
+
+  //handle scroll funtions
+  useEffect( () => {
+    const handleScroll = () =>{
+      const offset = window.scrollY;
+      if(offset > 0){
+        setSticky(true)
+      }else{
+        setSticky(false)
+      }
+    };
+    window.addEventListener("scroll",handleScroll);
+    return() =>{
+      window.addEventListener("scroll",handleScroll);
+
+    }
+
+  },[])
   const navItems = (
     <>
       <li>
@@ -45,8 +65,8 @@ export const Navbar = () => {
     </>
   );
   return (
-    <header className="max-w-screen-2xl container mx-auto">
-      <div className="navbar xl:px-24">
+    <header className="max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300">
+      <div className={`navbar xl:px-24 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" :""}`}>
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -77,12 +97,58 @@ export const Navbar = () => {
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            {navItems}
-            </ul>
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+
+          {/* Search btn */}
+          <button className="btn btn-ghost btn-circle hidden lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+
+          {/* cart Items */}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle mr-3 lg:flex hidden  items-center"
+          >
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+
+          {/* btn */}
+          <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-2">
+            <TbPhoneCall />
+            Contact
+          </a>
         </div>
       </div>
     </header>
